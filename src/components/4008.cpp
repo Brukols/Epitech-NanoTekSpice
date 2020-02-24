76f45cac64b16a6ecef5477f904b30768c4761fc
Amaury <amaury.lecomte@epitech.eu>
2020-02-24 11:26:02 +0100

    Fix 4008 problem

--- src/components/4008.cpp
+++ src/components/4008.cpp
@@ -26,22 +26,26 @@ nts::Tristate nts::C4008::sum(Tristate a, Tristate b, Tristate c) noexcept
         return (UNDEFINED);
     _tristatePin[13] = FALSE;
     if (a == TRUE)
-        sum = (sum == FALSE ? TRUE : FALSE);
+        sum = TRUE;
     if (b == TRUE)
         sum = (sum == FALSE ? TRUE : FALSE);
     if (c == TRUE)
         sum = (sum == FALSE ? TRUE : FALSE);
     if ((a == TRUE && b == TRUE) || (a == TRUE && c == TRUE) || (b == TRUE && c == TRUE))
-        _tristatePin[13] = TRUE; 
+        _tristatePin[13] = TRUE;
     return (sum);
 }
 
 void nts::C4008::run()
 {
-    _tristatePin[9] = sum(_tristatePin[6], _tristatePin[5], _tristatePin[8]);
-    _tristatePin[10] = sum(_tristatePin[4], _tristatePin[3], _tristatePin[9]);
-    _tristatePin[11] = sum(_tristatePin[2], _tristatePin[1], _tristatePin[10]);
-    _tristatePin[12] = sum(_tristatePin[0], _tristatePin[14], _tristatePin[11]);
-
-    updateOutput();
+    for (int i = 0; i < 3; i++) {
+        _tristatePin[9] = sum(_tristatePin[6], _tristatePin[5], _tristatePin[8]);
+        updateOutput();
+        _tristatePin[10] = sum(_tristatePin[4], _tristatePin[3], _tristatePin[9]);
+        updateOutput();
+        _tristatePin[11] = sum(_tristatePin[2], _tristatePin[1], _tristatePin[10]);
+        updateOutput();
+        _tristatePin[12] = sum(_tristatePin[0], _tristatePin[14], _tristatePin[11]);
+        updateOutput();
+    }
 }
