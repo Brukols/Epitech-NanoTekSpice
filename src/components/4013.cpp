7e98b22023dc93f8b9c8c6949bcd1780c8e5252d
Amaury <amaury.lecomte@epitech.eu>
2020-02-25 23:08:57 +0100

    Fix error

--- src/components/4013.cpp
+++ src/components/4013.cpp
@@ -19,7 +19,7 @@ nts::C4013::~C4013()
 {
 
 }
-#include <iostream>
+
 void nts::C4013::computeFlipFlop(size_t q1, size_t q2, size_t clock, size_t reset, size_t data, size_t set)
 {
     if (_tristatePin[set] == TRUE && _tristatePin[reset] == TRUE) {
@@ -37,7 +37,6 @@ void nts::C4013::computeFlipFlop(size_t q1, size_t q2, size_t clock, size_t rese
         _tristatePin[q2] = TRUE;
         return;
     }
-    std::cout << _clockStates[clock] << std::endl;
     if (_clockStates[clock] == LOW_TO_HIGH && _tristatePin[data] == TRUE) {
         _tristatePin[q1] = TRUE;
         _tristatePin[q2] = FALSE;
